1ba09ce9d56463d8cfb7a2b4da4e43e133b6a8b0
“philippe <philippe.mail.perso@gmail.com>
2026-04-10 16:13:37 +0200

    start

--- src/Frontend.php
+++ src/Frontend.php
@@ -24,12 +24,12 @@ class Frontend
 
     public static function init(): bool
     {
-        return self::status(My::checkContext(My::FRONTEND));
+        return FrontendTemplate::status(My::checkContext(My::FRONTEND));
     }
 
     public static function process(): bool
     {
-        if (!self::status()) {
+        if (!FrontendTemplate::status()) {
             return false;
         }
 
@@ -39,172 +39,29 @@ class Frontend
             return false;
         }
 
-        App::frontend()->template()->addBlock('BlogrollPage', self::BlogrollPage(...));
+        App::frontend()->template()->addBlock('BlogrollPage', FrontendTemplate::BlogrollPage(...));
         App::frontend()->template()->addValue('BlogrollPageTitle', FrontendTemplate::BlogrollPageTitle(...));
         App::frontend()->template()->addValue('BlogrollPageHeader', FrontendTemplate::BlogrollPageHeader(...));
-        App::frontend()->template()->addBlock('BlogrollPageIfTitle', self::IfTitle(...));
-        App::frontend()->template()->addBlock('BlogrollPageIfCategoryTitle', self::IfCategoryTitle(...));
-        App::frontend()->template()->addValue('BlogrollPageCategoryTitle', self::CategoryTitle(...));
-        App::frontend()->template()->addBlock('BlogrollPageLinks', self::Links(...));
-        App::frontend()->template()->addValue('BlogrollPageLink', self::Link(...));
-        App::frontend()->template()->addValue('BlogrollPageLinkTitle', self::LinkTitle(...));
-        App::frontend()->template()->addValue('BlogrollPageLinkHref', self::LinkHref(...));
-        App::frontend()->template()->addBlock('BlogrollPageIfLinkDesc', self::IfLinkDesc(...));
-        App::frontend()->template()->addValue('BlogrollPageLinkDesc', self::LinkDesc(...));
-        App::frontend()->template()->addBlock('BlogrollPageIfLinkLang', self::IfLinkLang(...));
-        App::frontend()->template()->addValue('BlogrollPageLinkLang', self::LinkLang(...));
-        App::frontend()->template()->addBlock('BlogrollPageIfLinkXFN', self::IfLinkXFN(...));
-        App::frontend()->template()->addValue('BlogrollPageLinkXFN', self::LinkXFN(...));
+        App::frontend()->template()->addBlock('BlogrollPageIfTitle', FrontendTemplate::IfTitle(...));
+        App::frontend()->template()->addBlock('BlogrollPageIfCategoryTitle', FrontendTemplate::IfCategoryTitle(...));
+        App::frontend()->template()->addValue('BlogrollPageCategoryTitle', FrontendTemplate::CategoryTitle(...));
+        App::frontend()->template()->addBlock('BlogrollPageLinks', FrontendTemplate::Links(...));
+        App::frontend()->template()->addValue('BlogrollPageLink', FrontendTemplate::Link(...));
+        App::frontend()->template()->addValue('BlogrollPageLinkTitle', FrontendTemplate::LinkTitle(...));
+        App::frontend()->template()->addValue('BlogrollPageLinkHref', FrontendTemplate::LinkHref(...));
+        App::frontend()->template()->addBlock('BlogrollPageIfLinkDesc', FrontendTemplate::IfLinkDesc(...));
+        App::frontend()->template()->addValue('BlogrollPageLinkDesc', FrontendTemplate::LinkDesc(...));
+        App::frontend()->template()->addBlock('BlogrollPageIfLinkLang', FrontendTemplate::IfLinkLang(...));
+        App::frontend()->template()->addValue('BlogrollPageLinkLang', FrontendTemplate::LinkLang(...));
+        App::frontend()->template()->addBlock('BlogrollPageIfLinkXFN', FrontendTemplate::IfLinkXFN(...));
+        App::frontend()->template()->addValue('BlogrollPageLinkXFN', FrontendTemplate::LinkXFN(...));
 
         App::behavior()->addBehaviors([
-            'publicBreadcrumb' => self::publicBreadcrumb(...),
+            'publicBreadcrumb' => FrontendTemplate::publicBreadcrumb(...),
 
             'initWidgets' => Widgets::initWidgets(...),
         ]);
 
         return true;
-    }
-
-    public static function publicBreadcrumb($context, $separator)
-    {
-        if ($context == 'blogrollpage') {
-            if (App::frontend()->context()->blogrollpage) {
-                $u = App::blog()->url() . App::url()->getURLFor('blogrollpage');
-
-                return '<a href="' . $u . '">' . __('Links') . '</a>' . $separator . App::frontend()->context()->blogrollpage;
-            }
-
-            return __('Links');
-        }
-    }
-
-    public static function BlogrollPage($attr, $content)
-    {
-        $res = '<?php foreach (App::frontend()->context()->blogrollpage_blogroll as $category => $links) { ?>';
-        $res .= $content;
-        $res .= '<?php } ?>';
-
-        return $res;
-    }
-
-    public static function IfTitle($attr, $content)
-    {
-        $res = '<?php $brp_cat = App::frontend()->context()->blogrollpage; if (!empty($brp_cat)) { ?>';
-        $res .= $content;
-        $res .= '<?php } ?>';
-
-        return $res;
-    }
-
-    public static function IfCategoryTitle($attr, $content)
-    {
-        $res = '<?php if (!empty($category)) { ?>';
-        $res .= $content;
-        $res .= '<?php } ?>';
-
-        return $res;
-    }
-
-    public static function CategoryTitle($attr)
-    {
-        $f = App::frontend()->template()->getFilters($attr);
-
-        return '<?php echo ' . sprintf($f, '$category') . '; ?>';
-    }
-
-    public static function Links($attr, $content)
-    {
-        $res = '<?php foreach ($links as $link) { ?>';
-        $res .= $content;
-        $res .= '<?php } ?>';
-
-        return $res;
-    }
-
-    public static function Link($attr)
-    {
-        return '<?php echo ' . self::class . '::makeLink($link,App::blog()->settings()->blogrollpage->blogrollpage_new_window); ?>';
-    }
-
-    public static function makeLink($link, $new_window = null)
-    {
-        $title = $link['link_title'];
-        $href  = $link['link_href'];
-        $desc  = $link['link_desc'];
-        $lang  = $link['link_lang'];
-        $xfn   = $link['link_xfn'];
-
-        $link = '<a href="' . Html::escapeHTML($href) . '"' .
-        ((!$lang) ? '' : ' hreflang="' . Html::escapeHTML($lang) . '"') .
-        ((!$desc) ? '' : ' title="' . Html::escapeHTML($desc) . '"') .
-        ((!$xfn) ? '' : ' rel="' . Html::escapeHTML($xfn) . '"') .
-        ((!$new_window) ? '' : ' onclick="window.open(this.href); return false;"') .
-        '>' .
-        Html::escapeHTML($title) .
-        '</a>';
-
-        return $link;
-    }
-
-    public static function LinkTitle($attr)
-    {
-        $f = App::frontend()->template()->getFilters($attr);
-
-        return '<?php echo ' . sprintf($f, '$link[\'link_title\']') . '; ?>';
-    }
-
-    public static function LinkHref($attr)
-    {
-        $f = App::frontend()->template()->getFilters($attr);
-
-        return '<?php echo ' . sprintf($f, '$link[\'link_href\']') . '; ?>';
-    }
-
-    public static function IfLinkDesc($attr, $content)
-    {
-        $res = '<?php if (!empty($link[\'link_desc\'])) { ?>';
-        $res .= $content;
-        $res .= '<?php } ?>';
-
-        return $res;
-    }
-
-    public static function LinkDesc($attr)
-    {
-        $f = App::frontend()->template()->getFilters($attr);
-
-        return '<?php echo ' . sprintf($f, '$link[\'link_desc\']') . '; ?>';
-    }
-
-    public static function IfLinkLang($attr, $content)
-    {
-        $res = '<?php if (!empty($link[\'link_lang\'])) { ?>';
-        $res .= $content;
-        $res .= '<?php } ?>';
-
-        return $res;
-    }
-
-    public static function LinkLang($attr)
-    {
-        $f = App::frontend()->template()->getFilters($attr);
-
-        return '<?php echo ' . sprintf($f, '$link[\'link_lang\']') . '; ?>';
-    }
-
-    public static function IfLinkXFN($attr, $content)
-    {
-        $res = '<?php if (!empty($link[\'link_xfn\'])) { ?>';
-        $res .= $content;
-        $res .= '<?php } ?>';
-
-        return $res;
-    }
-
-    public static function LinkXFN($attr)
-    {
-        $f = App::frontend()->template()->getFilters($attr);
-
-        return '<?php echo ' . sprintf($f, '$link[\'link_xfn\']') . '; ?>';
     }
 }
--- src/FrontendTemplate.php
+++ src/FrontendTemplate.php
@@ -49,4 +49,147 @@ class FrontendTemplate
             attr: $attr,
         );
     }
+
+    public static function publicBreadcrumb($context, $separator)
+    {
+        if ($context == 'blogrollpage') {
+            if (App::frontend()->context()->blogrollpage) {
+                $u = App::blog()->url() . App::url()->getURLFor('blogrollpage');
+
+                return '<a href="' . $u . '">' . __('Links') . '</a>' . $separator . App::frontend()->context()->blogrollpage;
+            }
+
+            return __('Links');
+        }
+    }
+
+    public static function BlogrollPage($attr, $content)
+    {
+        $res = '<?php foreach (App::frontend()->context()->blogrollpage_blogroll as $category => $links) { ?>';
+        $res .= $content;
+        $res .= '<?php } ?>';
+
+        return $res;
+    }
+
+    public static function IfTitle($attr, $content)
+    {
+        $res = '<?php $brp_cat = App::frontend()->context()->blogrollpage; if (!empty($brp_cat)) { ?>';
+        $res .= $content;
+        $res .= '<?php } ?>';
+
+        return $res;
+    }
+
+    public static function IfCategoryTitle($attr, $content)
+    {
+        $res = '<?php if (!empty($category)) { ?>';
+        $res .= $content;
+        $res .= '<?php } ?>';
+
+        return $res;
+    }
+
+    public static function CategoryTitle($attr)
+    {
+        $f = App::frontend()->template()->getFilters($attr);
+
+        return '<?php echo ' . sprintf($f, '$category') . '; ?>';
+    }
+
+    public static function Links($attr, $content)
+    {
+        $res = '<?php foreach ($links as $link) { ?>';
+        $res .= $content;
+        $res .= '<?php } ?>';
+
+        return $res;
+    }
+
+    public static function Link($attr)
+    {
+        return '<?php echo ' . self::class . '::makeLink($link,App::blog()->settings()->blogrollpage->blogrollpage_new_window); ?>';
+    }
+
+    public static function makeLink($link, $new_window = null)
+    {
+        $title = $link['link_title'];
+        $href  = $link['link_href'];
+        $desc  = $link['link_desc'];
+        $lang  = $link['link_lang'];
+        $xfn   = $link['link_xfn'];
+
+        $link = '<a href="' . Html::escapeHTML($href) . '"' .
+        ((!$lang) ? '' : ' hreflang="' . Html::escapeHTML($lang) . '"') .
+        ((!$desc) ? '' : ' title="' . Html::escapeHTML($desc) . '"') .
+        ((!$xfn) ? '' : ' rel="' . Html::escapeHTML($xfn) . '"') .
+        ((!$new_window) ? '' : ' onclick="window.open(this.href); return false;"') .
+        '>' .
+        Html::escapeHTML($title) .
+        '</a>';
+
+        return $link;
+    }
+
+    public static function LinkTitle($attr)
+    {
+        $f = App::frontend()->template()->getFilters($attr);
+
+        return '<?php echo ' . sprintf($f, '$link[\'link_title\']') . '; ?>';
+    }
+
+    public static function LinkHref($attr)
+    {
+        $f = App::frontend()->template()->getFilters($attr);
+
+        return '<?php echo ' . sprintf($f, '$link[\'link_href\']') . '; ?>';
+    }
+
+    public static function IfLinkDesc($attr, $content)
+    {
+        $res = '<?php if (!empty($link[\'link_desc\'])) { ?>';
+        $res .= $content;
+        $res .= '<?php } ?>';
+
+        return $res;
+    }
+
+    public static function LinkDesc($attr)
+    {
+        $f = App::frontend()->template()->getFilters($attr);
+
+        return '<?php echo ' . sprintf($f, '$link[\'link_desc\']') . '; ?>';
+    }
+
+    public static function IfLinkLang($attr, $content)
+    {
+        $res = '<?php if (!empty($link[\'link_lang\'])) { ?>';
+        $res .= $content;
+        $res .= '<?php } ?>';
+
+        return $res;
+    }
+
+    public static function LinkLang($attr)
+    {
+        $f = App::frontend()->template()->getFilters($attr);
+
+        return '<?php echo ' . sprintf($f, '$link[\'link_lang\']') . '; ?>';
+    }
+
+    public static function IfLinkXFN($attr, $content)
+    {
+        $res = '<?php if (!empty($link[\'link_xfn\'])) { ?>';
+        $res .= $content;
+        $res .= '<?php } ?>';
+
+        return $res;
+    }
+
+    public static function LinkXFN($attr)
+    {
+        $f = App::frontend()->template()->getFilters($attr);
+
+        return '<?php echo ' . sprintf($f, '$link[\'link_xfn\']') . '; ?>';
+    }
 }
